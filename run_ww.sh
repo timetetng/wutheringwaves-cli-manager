565a2e134fac0e6d48b19b29b44ecfa01097ab42
timetetng <timetetng@gmail.com>
2025-11-22 12:07:01 +0800

    fix: 启动脚本添加注释

--- run_ww.sh
+++ run_ww.sh
@@ -6,14 +6,15 @@ PROTON_BIN="$HOME/.local/share/Steam/compatibilitytools.d/GE-Proton10-25/proton"
 # 游戏路径
 GAME_EXE="$HOME/share/WutheringWaves/Wuthering Waves bilibili/Wuthering Waves Game/Client/Binaries/Win64/Client-Win64-Shipping.exe"
 
-# 兼容层路径以及steam游戏目录
+# steam 游戏目录路径
 export STEAM_COMPAT_DATA_PATH="$HOME/.local/share/Steam/steamapps/compatdata/3139039821"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/Steam"
 
 # 环境变量
 export steamdeck=1
+# 用于帧率显示(sudo pacman -S mangohud 安装)
 export MANGOHUD=1
-# === 关键修改 ===
+
 # 定义 Steam 内部自带的 Runtime 脚本路径
 STEAM_RUNTIME_SCRIPT="$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh"
 
