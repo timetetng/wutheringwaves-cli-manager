8107028c70af511f228a838a7f1fe43fcaf9c8f3
timetetng <timetetng@gmail.com>
2025-12-23 14:44:03 +0800

    fix

--- run_ww.sh
+++ run_ww.sh
@@ -13,7 +13,7 @@ export STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.local/share/Steam"
 # 环境变量
 export STEAMDESK=1
 # 用于帧率显示(sudo pacman -S mangohud 安装)
-export MANGOHUD=1
+#export MANGOHUD=1
 
 # 定义 Steam 内部自带的 Runtime 脚本路径
 STEAM_RUNTIME_SCRIPT="$HOME/.local/share/Steam/ubuntu12_32/steam-runtime/run.sh"
